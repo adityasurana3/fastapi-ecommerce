91f206e8003e42be1ca6625608b80da0938c9b2c
Aditya Surana <suranaaditya3@gmail.com>
2026-09-19 17:39:13 +0545

    category update and delete

--- backend/app/product/routers.py
+++ backend/app/product/routers.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
 
 from app.db.config import SessionDep
 from app.product.schema import CategoryOut, CategoryCreate
-from app.product.services import category_create
+from app.product.services import category_create, fetch_all_categories, remove_category
 from app.accounts.models import User
-from fastapi import Depends
 from app.accounts.dependencies import require_admin
+from fastapi.exceptions import HTTPException
 
 router = APIRouter()
 
@@ -15,3 +15,19 @@ async def create_category(
     session: SessionDep, category: CategoryCreate, admin: User = Depends(require_admin)
 ):
     return await category_create(session, category)
+
+
+@router.get("/categories", response_model=list[CategoryOut])
+async def list_categories(session: SessionDep) -> list[CategoryOut]:
+    return await fetch_all_categories(session)
+
+
+@router.delete("/delete-category/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_category(
+    session: SessionDep, category_id: int, user: User = Depends(require_admin)
+):
+    result = remove_category(session, category_id)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
+        )
--- backend/app/product/services.py
+++ backend/app/product/services.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.product.schema import CategoryOut, CategoryCreate
 from app.product.models import Category
+from sqlalchemy import select
 
 
 async def category_create(
@@ -11,3 +12,18 @@ async def category_create(
     await session.commit()
     await session.refresh(category)
     return category
+
+
+async def fetch_all_categories(session: AsyncSession) -> list[CategoryOut]:
+    categories = await session.scalars(select(Category))
+    return categories.all()
+
+
+async def remove_category(session: AsyncSession, category_id: int) -> bool:
+    session.get(Category, category_id)
+    category = await session.get(Category, category_id)
+    if not category:
+        return False
+    await session.delete(category)
+    await session.commit()
+    return True
